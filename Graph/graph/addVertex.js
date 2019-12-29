00861caa9e398e1d8f5ca0b3aa51b133c79e3f1a
b117014 <b117014@iiit-bh.ac.in>
2019-12-29 12:47:43 +0530

    indirected grapg

--- Graph/graph/addVertex.js
+++ Graph/graph/addVertex.js
@@ -8,4 +8,18 @@ class Graph{
     addVertex(vertex){
         if(!this.adjacencyList[vertex]){this.adjacencyList[vertex]=[]}
     }
+    addEdge(v1, v2){
+        if(this.adjacencyList[v1]!==v2){
+               this.adjacencyList[v1].push(v2);
+        }
+        if(this.adjacencyList[v2]!==v1){
+            this.adjacencyList[v2].push(v1);
+     }
+    }
 }
+
+let graph = new Graph();
+graph.addVertex("kolkata")
+graph.addVertex("bhubaneswar");
+graph.addEdge("kolkata","bhubaneswar")
+console.log(graph.adjacencyList);
